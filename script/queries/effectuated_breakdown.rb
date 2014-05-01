e269e1dd18995f7cc2d3c11831cbea296afedf69
Trey Evans <lewis.r.evans@gmail.com>
2014-05-01 18:45:57 -0400

    Query fixes.
    I discovered that the "$unwind" operation doesn't quite do what I
    thought it did.  Replacing it with a more straightforward approach gave
    the correct numbers.

--- script/queries/effectuated_breakdown.rb
+++ script/queries/effectuated_breakdown.rb
@@ -4,17 +4,32 @@ policy_ids = Protocols::X12::TransactionSetHeader.collection.aggregate(
 ).map { |r| r["_id"] }.uniq
 
 people = Policy.collection.aggregate(
-  {"$match" => {"_id" => {"$in" => policy_ids}, "aasm_state" => {"$nin" => ["cancelled", "terminated"]}}},
-  {"$unwind" => "$enrollees"},
-  {"$group" => {"_id" => "1", "enrollees" => {"$addToSet" => "$enrollees.m_id"}}},
-  {"$unwind" => "$enrollees"},
-  {"$group" => {"_id" => "1", "enrollees" => {"$sum" => 1}}}
-#  {"$sort" => {"enrollees" => -1}},
-).first
+  {"$match" => {"_id" => {"$in" => policy_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))},
+  {"$group" => {"_id" => "1", "enrollee_list" => {"$addToSet" => "$enrollees.m_id"}}},
+).first['enrollee_list'].uniq
 
 policies = Policy.where(
-  {"_id" => {"$in" => policy_ids}, "aasm_state" => {"$nin" => ["cancelled", "terminated"]}}
-).count
+  {"_id" => {"$in" => policy_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))
+)
 
-puts "Effectuated Policies: #{policies}"
-puts "Effectuated Lives: #{people['enrollees']}"
+dental_plan_ids = Plan.where({"coverage_type" => "dental"}).map(&:id)
+
+dental_policies = Policy.where(
+  {"_id" => {"$in" => policy_ids}, "plan_id" => { "$in" => dental_plan_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))
+)
+
+puts dental_policies.last.enrollees.first.person.id
+
+m_ids = []
+
+policies.each do |pol|
+  m_ids << pol.enrollees.map(&:m_id)
+end
+
+m_ids.uniq!
+
+
+puts "Effectuated Policies: #{policies.count}"
+puts "Effectuated Lives: #{people.length}"
+puts "Effectuated Lives (counted the hard way): #{m_ids.length}"
+puts "Dental Policies: #{dental_policies.count}"
