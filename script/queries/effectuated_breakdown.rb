3aecc58ac56148b1090c3b0c794338694293b031
Trey Evans <lewis.r.evans@gmail.com>
2014-05-01 21:06:19 -0400

    Query fixes.
    I really don't see anything wrong with this query. It seems to return
    the right number of members.

--- script/queries/effectuated_breakdown.rb
+++ script/queries/effectuated_breakdown.rb
@@ -3,33 +3,24 @@ policy_ids = Protocols::X12::TransactionSetHeader.collection.aggregate(
   {"$group" => {"_id" => "$policy_id"}}
 ).map { |r| r["_id"] }.uniq
 
-people = Policy.collection.aggregate(
-  {"$match" => {"_id" => {"$in" => policy_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))},
-  {"$group" => {"_id" => "1", "enrollee_list" => {"$addToSet" => "$enrollees.m_id"}}},
-).first['enrollee_list'].uniq
+puts policy_ids.length.inspect
+
+dental_plan_ids = Plan.where({"coverage_type" => "dental"}).map(&:id).map { |tid| Moped::BSON::ObjectId.from_string(tid) }
 
 policies = Policy.where(
-  {"_id" => {"$in" => policy_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))
+  {"_id" => {"$in" => policy_ids}, "plan_id" => { "$nin" => dental_plan_ids},  "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))
 )
-
-dental_plan_ids = Plan.where({"coverage_type" => "dental"}).map(&:id)
-
-dental_policies = Policy.where(
-  {"_id" => {"$in" => policy_ids}, "plan_id" => { "$in" => dental_plan_ids}, "eg_id" => {"$not" => /DC0.{32}/}}.merge(Policy.active_as_of_expression(Time.now))
-)
-
-puts dental_policies.last.enrollees.first.person.id
 
 m_ids = []
 
 policies.each do |pol|
-  m_ids << pol.enrollees.map(&:m_id)
+  pol.enrollees.each do |en|
+    m_ids << en.m_id
+  end
 end
 
 m_ids.uniq!
 
 
 puts "Effectuated Policies: #{policies.count}"
-puts "Effectuated Lives: #{people.length}"
-puts "Effectuated Lives (counted the hard way): #{m_ids.length}"
-puts "Dental Policies: #{dental_policies.count}"
+puts "Effectuated Lives: #{m_ids.uniq.length}"
