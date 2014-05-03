76e82b205d01ebe6f59669526157be7de3d40ea1
Dan Thomas <dan.thomas@ipublic.org>
2014-05-03 15:20:38 -0400

    Add fields and validations

--- app/models/eligibility.rb
+++ app/models/eligibility.rb
@@ -1,3 +1,12 @@
 class Eligibility
   include Mongoid::Document
+
+	field :date_determined, type: Date
+  field :max_aptc, type: BigDecimal, default: 0.00
+  field :csr_amt, type: BigDecimal, default: 0.00
+
+  embedded_in :tax_household
+
+  validates_presence_of :date_determined, :max_aptc, :csr_amt
+
 end
