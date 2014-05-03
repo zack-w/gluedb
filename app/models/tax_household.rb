1f741751e98393f0738a4a9f1a0ac92e7723faf6
Dan Thomas <dan.thomas@ipublic.org>
2014-05-03 15:21:37 -0400

    Add enrollment state machine

--- app/models/tax_household.rb
+++ app/models/tax_household.rb
@@ -3,20 +3,46 @@ class TaxHousehold
   include Mongoid::Timestamps
   include Mongoid::Versioning
   include Mongoid::Paranoia
+  include AASM
 
-  # A tax household
 
   field :rel, as: :relationship, type: String
-  field :aptc_total, type: BigDecimal, default: 0.00
-  field :csr_amt, type: BigDecimal, default: 0.00
+  field :aasm_state, type: String
 
-  validates_presence_of :rel
-	validates_inclusion_of :rel, in: ["self", "spouse", "dependent"]
+  validates :rel, presence: true, inclusion: {in: %w( self spouse child ward life_partner )}
 
+  index({:aasm_state => 1})
+
+  belongs_to :family, counter_cache: true
   has_many :people, dependent: :delete
   embeds_many :special_enrollment_periods
   embeds_many :eligibilities
 
+  # Value from latest eligibility determination
+  def max_aptc
+  end
 
+  # Value from latest eligibility determination
+  def csr
+  end
+
+  aasm do
+    state :closed_enrollment, initial: true
+    state :open_enrollment_period
+    state :special_enrollment_period
+
+    event :open_enrollment do
+      transitions from: [:closed_enrollment, :special_enrollment_period], to: :open_enrollment_period
+    end
+
+    # TODO - what are rules around special enrollments that extend past open enrollment?
+    event :special_enrollment do
+      transitions from: [:closed_enrollment, :open_enrollment], to: :special_enrollment_period
+    end
+
+    event :close_enrollment do
+      transitions from: [:open_enrollment, :special_enrollment_period], to: :closed_enrollment
+    end
+  end
 
 end
