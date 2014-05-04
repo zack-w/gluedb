99ba5c33e78e53f04eed09287efa38e99887673b
Dan Thomas <dan.thomas@ipublic.org>
2014-05-04 12:20:56 -0400

    Add validations

--- app/models/eligibility.rb
+++ app/models/eligibility.rb
@@ -3,10 +3,16 @@ class Eligibility
 
 	field :date_determined, type: Date
   field :max_aptc, type: BigDecimal, default: 0.00
-  field :csr_amt, type: BigDecimal, default: 0.00
+  field :csr_percent, type: BigDecimal, default: 0.00
 
   embedded_in :household
 
-  validates_presence_of :date_determined, :max_aptc, :csr_amt
+  validates_presence_of :date_determined, :max_aptc, :csr_percent
+  validate :csr_as_percent
+
+	# Validate csr_percent value is in range 1..0
+  def csr_as_percent
+		errors.add(:csr_percent, "value must be between 0 and 1") unless (0 <= csr_percent && csr_percent <= 1)
+  end
 
 end
