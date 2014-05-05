9397b97ec845b5a8f97e11666e28cb1999d5a018
Dan Thomas <dan.thomas@ipublic.org>
2014-05-04 22:46:39 -0400

    Add methods to access active/current SEPs

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,4 +1,4 @@
-class TaxHousehold
+class Household
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Versioning
@@ -9,21 +9,37 @@ class TaxHousehold
   field :rel, as: :relationship, type: String
   field :aasm_state, type: String
 
-  validates :rel, presence: true, inclusion: {in: %w( self spouse child ward life_partner )}
+  validates :rel, presence: true, inclusion: {in: %w( subscriber responsible_party spouse life_partner child ward )}
 
   index({:aasm_state => 1})
 
   belongs_to :family, counter_cache: true
   has_many :people, dependent: :delete
-  embeds_many :special_enrollment_periods
+  embeds_many :special_enrollment_periods, cascade_callbacks: true
   embeds_many :eligibilities
 
-  # Value from latest eligibility determination
-  def max_aptc
+  # List of SEPs active for this Household on this or specified date
+  def active_seps(day = Date.today)
+    special_enrollment_periods.find_all { |sep| (sep.start_date..sep.end_date).include?(day) }
+  end
+
+  # single SEP with latest end date from list of active SEPs
+  def current_sep
+    active_seps.max { |sep| sep.end_date }
+  end
+
+  def current_eligibility
+    eligibilities.max_by { |e| e.date_determined }
   end
 
   # Value from latest eligibility determination
-  def csr
+  def max_aptc
+    current_eligibility.max_aptc
+  end
+
+  # Value from latest eligibility determination
+  def csr_percent
+    current_eligibility.csr_percent
   end
 
   aasm do
@@ -32,16 +48,16 @@ class TaxHousehold
     state :special_enrollment_period
 
     event :open_enrollment do
-      transitions from: [:closed_enrollment, :special_enrollment_period], to: :open_enrollment_period
+      transitions from: [:closed_enrollment, :special_enrollment_period, :open_enrollment_period], to: :open_enrollment_period
     end
 
     # TODO - what are rules around special enrollments that extend past open enrollment?
     event :special_enrollment do
-      transitions from: [:closed_enrollment, :open_enrollment], to: :special_enrollment_period
+      transitions from: [:closed_enrollment, :open_enrollment_period, :special_enrollment_period], to: :special_enrollment_period
     end
 
     event :close_enrollment do
-      transitions from: [:open_enrollment, :special_enrollment_period], to: :closed_enrollment
+      transitions from: [:open_enrollment_period, :special_enrollment_period, :closed_enrollment], to: :closed_enrollment
     end
   end
 
