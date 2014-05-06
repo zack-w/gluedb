63c6edec401db9e917996f56ae1f8215de947ca2
Trey Evans <lewis.r.evans@gmail.com>
2014-05-06 10:55:25 -0400

    Better factoring of importer.

--- app/models/parsers/edi/etf/person_loop.rb
+++ app/models/parsers/edi/etf/person_loop.rb
@@ -11,6 +11,34 @@ module Parsers
               r[1] == "17"
             end)[2]            
         end
+
+        def carrier_member_id
+          # TODO: Memoize this, but account for nil
+          get_carrier_member_id
+        end
+
+        def get_carrier_member_id
+            c_member_seg = (@loop["REFs"].detect do |r|
+              r[1] == "23"
+            end)
+            c_member_seg.nil? ? nil : c_member_seg[2]
+        end
+
+        def rel_code
+          @rel_code ||= @loop["INS"][2]
+        end
+
+        def ben_stat
+          @ben_stat ||= @loop["INS"][5]
+        end
+
+        def emp_stat
+          @emp_stat ||= @loop["INS"][8]
+        end
+
+        def subscriber?
+          @subscriber ||= (@loop["INS"][2].strip == "18")
+        end
       end
     end
   end
--- spec/models/parsers/edi/etf/person_loop_spec.rb
+++ spec/models/parsers/edi/etf/person_loop_spec.rb
@@ -1,12 +1,23 @@
 require_relative File.join("..", "..", "..", "..", "spec_helper")
 
-describe Parsers::Edi::Etf::PersonLoop do
+describe Parsers::Edi::Etf::PersonLoop, "
+given:
+ - a member id
+ - a relationship code
+ - a benefit status
+ - an employment status
+ - a non-subscriber relationship code
+" do
   include EdiFactory
 
   let(:the_member_id) { "the member id" }
+  let(:the_relationship_code) { "the relationship code" }
+  let(:the_benefit_status_code) { "the benefit status code" }
+  let(:the_employment_status_code) { "the employment status code" }
 
   let(:l2000) {
     {
+          "INS" => [0, nil, the_relationship_code, nil, nil, the_benefit_status_code, nil, nil, the_employment_status_code],
           "REFs" => [
             member_id_ref_segment(the_member_id)
           ] 
@@ -16,5 +27,40 @@ describe Parsers::Edi::Etf::PersonLoop do
   subject { Parsers::Edi::Etf::PersonLoop.new(l2000)  }
 
   its(:member_id) { should eq(the_member_id) }
+  its(:carrier_member_id) { should be_nil }
+  its(:rel_code) { should eq(the_relationship_code) }
+  its(:ben_stat) { should eq(the_benefit_status_code) }
+  its(:emp_stat) { should eq(the_employment_status_code) }
+
+  it { should_not be_subscriber }
+
+end
+
+
+describe Parsers::Edi::Etf::PersonLoop, "
+given:
+ - a carrier_member_id
+ - a subscriber relationship code
+ - no employment status, but an INS segment
+" do
+  include EdiFactory
+
+  let(:carrier_member_id) { "the carrier member id" }
+
+  let(:l2000) {
+    {
+          "INS" => [0, nil, "18", nil, nil],
+          "REFs" => [
+            carrier_member_id_ref_segment(carrier_member_id)
+          ] 
+    }
+  }
+
+  subject { Parsers::Edi::Etf::PersonLoop.new(l2000)  }
+
+  its(:carrier_member_id) { should eq(carrier_member_id) }
+  its(:emp_stat) { should be_nil }
+
+  it { should be_subscriber }
 
 end
