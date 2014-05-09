242a914a76dbcf3915c1d785105d00c150aa540f
Trey Evans <lewis.r.evans@gmail.com>
2014-05-09 15:48:22 -0400

    Finished simple request reply event submission.

--- app/models/protocols/amqp/person_update_handler.rb
+++ app/models/protocols/amqp/person_update_handler.rb
@@ -1,33 +1,56 @@
 require 'builder'
+require 'timeout'
 
 module Protocols
   module Amqp
     class PersonUpdateHandler
+      include Timeout
+
       def handle_update(obj, delta)
         xml = ::Builder::XmlMarkup.new(:indent => 2)
         xml.instruct!(:xml, :encoding => "UTF-8")
-        xml.person_changes({"xmlns" => "uri:gluedb:v0.1.0:person"}) do |xml|
+        message_ns = "urn:gluedb:v0.1.0:person:update"
+        xml.person_changes({"xmlns" => message_ns}) do |xml|
           obj.to_xml(:builder => xml, :dasherize => false, :root => "person", :skip_instruct => "true")
           delta.to_xml(:builder => xml, :dasherize => false, :root => "changes", :skip_instruct => "true")
         end
-#        ex_type, ex_name = Protocols::Amqp::Settings.gluedb_onramp_exchange
-#        key = Protocols::Amqp::Settings.onramp_routing_key("individual", "update")
-        transform_xsl = ""
-        raise xml.target!
-        event_exchange = Protocols::Amqp::Settings.event_exchange
-        event_routing_key = Protocols::Amqp::Settings.event_key("individual", "update")
+        transform_exchange = Protocols::Amqp::Settings.transformation_exchange
+        transform_key = Protocols::Amqp::Settings.transformation_key
         conn = Bunny.new
         conn.start
         ch = conn.create_channel
-        ex = ch.send(*event_exchange)
+        ex = ch.send(*transform_exchange)
+        reply_queue = ch.queue
         # TODO: send to the right place, with the transformation XSL
         ex.publish( xml.target!,
-                    :routing_key => key,
-                    :reply_to => event_routing_key,
+                    :durable => true,
+                    :routing_key => transform_key,
+                    :reply_to => reply_queue.name,
                     :headers => {
-                      :transform => transform_xsl
+                      :transform => message_ns
                     }
                   )
+        reply_properties = nil
+        reply_result = nil
+        timeout(5) {
+          consumer = Bunny::Consumer.new(ch, reply_queue)
+          consumer.on_delivery do |delivery_info, properties, payload|
+            reply_properties = properties
+            reply_result = payload
+            consumer.cancel
+            throw :terminate, "cancelled because of response"
+          end
+          reply_queue.subscribe_with(consumer, :block => true)
+        }
+        if !(reply_properties.headers["result_code"] == "OK")
+          reply_queue.delete
+          raise reply_properties.headers["result_code"].inspect
+        end
+        event_exchange = Protocols::Amqp::Settings.event_exchange("individual")
+        event_key = Protocols::Amqp::Settings.event_key("individual", "update")
+        e_ex = ch.send(*event_exchange)
+        e_ex.publish(reply_result, :durable => true, :routing_key => event_key)
+        reply_queue.delete
         conn.close 
       end
     end
