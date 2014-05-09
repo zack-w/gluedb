bc36393d0e2e7d8c66b7103201b571ecc61f3d22
John Kisor <jkisor@gmail.com>
2014-05-09 12:04:45 -0400

    extract method object for RawMember#check_for_member, test it, use it. (b/c RawMember is difficult to create in tests)

---
+++ app/queries/member_by_hbx_id_query.rb
@@ -0,0 +1,11 @@
+class MemberByHBXIDQuery
+  def initialize(dcas_no)
+    @dcas_no = dcas_no
+  end
+
+  def execute
+    person = Person.where("members.hbx_member_id" => @dcas_no).first
+    return(nil) if person.blank?
+    person.nil? ? nil : (person.members.detect { |m| m.hbx_member_id == @dcas_no}) 
+  end
+end
