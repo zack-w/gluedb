0f493ce131b4932239ca2fa360d65098e6ed70b6
Dan Thomas <dan.thomas@ipublic.org>
2014-05-13 16:48:18 -0400

    Refactor household with application_group model

---
+++ app/models/person_relationship.rb
@@ -0,0 +1,27 @@
+class PersonRelationship
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  MALE_RELATIONSHIPS_LIST   = %W[spouse father grandfather grandson uncle nephew cousin adopted\ child
+                              foster\ child son-in-law brother-in-law father-in-law brother ward 
+                              stepparent stepson child sponsored\ dependent dependent\ of\ a\ minor\ dependent 
+                              ex-spouse guardian court\ appointed\ guardian collateral\ dependent life\ partner]
+
+  FEMALE_RELATIONSHIPS_LIST = %W[spouse mother grandmother granddaughter aunt niece cousin adopted\ child 
+                              foster\ child daughter-in-law sister-in-law mother-in-law sister ward 
+                              stepparent stepdaughter child sponsored\ dependent dependent\ of\ a\ minor\ dependent
+                              ex-spouse guardian court\ appointed\ guardian collateral\ dependent life\ partner]
+
+  ALL_RELATIONSHIPS_LIST    = male_relationships_list & female_relationships_list
+
+  field :subject_person, type: ObjectId
+  field :relationship_kind, type: String
+  field :primary_person, ObjectId
+
+	validates_presence_of :subject_person, :relationship_kind, :primary_person
+	validates_inclusion_of :relationship_kind, in: ALL_RELATIONSHIPS_LIST
+
+  embedded_in :household
+
+
+end
