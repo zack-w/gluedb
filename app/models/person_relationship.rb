fab76b5ab3c6b1c84c2f7e4e0b229397e36b5b11
Dan Thomas <dan.thomas@ipublic.org>
2014-05-18 11:26:38 -0400

    Specify relationship semantics and factor 'symmetrical relationships' into separate list

--- app/models/person_relationship.rb
+++ app/models/person_relationship.rb
@@ -2,23 +2,27 @@ class PersonRelationship
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  MALE_RELATIONSHIPS_LIST   = %W[spouse father grandfather grandson uncle nephew cousin adopted\ child
-                              foster\ child son-in-law brother-in-law father-in-law brother ward 
-                              stepparent stepson child sponsored\ dependent dependent\ of\ a\ minor\ dependent 
-                              ex-spouse guardian court\ appointed\ guardian collateral\ dependent life\ partner]
+  MALE_RELATIONSHIPS_LIST   = %W[father grandfather grandson uncle nephew adopted\ child stepparent
+                              foster\ child son-in-law brother-in-law father-in-law brother ward
+                              stepson child sponsored\ dependent dependent\ of\ a\ minor\ dependent 
+                              guardian court\ appointed\ guardian collateral\ dependent life\ partner]
 
-  FEMALE_RELATIONSHIPS_LIST = %W[spouse mother grandmother granddaughter aunt niece cousin adopted\ child 
+  FEMALE_RELATIONSHIPS_LIST = %W[mother grandmother granddaughter aunt niece adopted\ child stepparent
                               foster\ child daughter-in-law sister-in-law mother-in-law sister ward 
-                              stepparent stepdaughter child sponsored\ dependent dependent\ of\ a\ minor\ dependent
-                              ex-spouse guardian court\ appointed\ guardian collateral\ dependent life\ partner]
+                              stepdaughter child sponsored\ dependent dependent\ of\ a\ minor\ dependent
+                              guardian court\ appointed\ guardian collateral\ dependent life\ partner]
 
-  ALL_RELATIONSHIPS_LIST    = MALE_RELATIONSHIPS_LIST & FEMALE_RELATIONSHIPS_LIST
+  SYMMETRICAL_RELATIONSHIPS_LIST = %W[head\ of\ household spouse ex-spouse cousin ward]
 
+  ALL_RELATIONSHIPS_LIST    =  SYMMETRICAL_RELATIONSHIPS_LIST & MALE_RELATIONSHIPS_LIST & FEMALE_RELATIONSHIPS_LIST
+
+  # Relationships are defined using RDF-style Subject -> Predicate -> Object
+  # Subject is 'head of household' by convention
   field :subject_person, type: Moped::BSON::ObjectId
   field :relationship_kind, type: String
-  field :primary_person, type: Moped::BSON::ObjectId
+  field :object_person, type: Moped::BSON::ObjectId
 
-	validates_presence_of :subject_person, :relationship_kind, :primary_person
+	validates_presence_of :subject_person, :relationship_kind, :object_person
 	validates_inclusion_of :relationship_kind, in: ALL_RELATIONSHIPS_LIST
 
   embedded_in :household
