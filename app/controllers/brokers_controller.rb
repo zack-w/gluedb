5d92e0099241c7852b1a0c344e44946ad89574d7
Zack W <zack13254@gmail.com>
2014-07-11 09:53:25 -0400

    Refactored broker code, fixed bug

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -1,26 +1,39 @@
 class BrokersController < ApplicationController
-  def index
-    @q = params[:q]
 
-    if params[:q].present?
-      @brokers = Broker.search(@q).page(params[:page]).per(12)
-    else
-      @brokers = Broker.page(params[:page]).per(12)
-    end
-    
+  # The index page- should show a listing of brokers (paginated)
+  def index
+    # See if the user passed in a query argument (via HTTP GET)
+    @q = params[:q];
+
+    # Query based on the query argument if there is one
+    @brokers = ( params[:q].present? && Broker.search(@q) || Broker ).page(params[:page]).per(12);
+
+    # Support both HTML and Json responses
     respond_to do |format|
 	    format.html # index.html.erb
 	    format.json { render json: @brokers }
 	  end
   end
-
+  
+  # Show details for a specific broker
   def show
-		@broker = Broker.find(params[:id])
+    @brokerQuery = Broker.where( id: params[ :id ] );
+    
+    # If the broker being queried for doesn't exist, throw a 404
+    if( 1 > @brokerQuery.count )then
+      render( :file => "#{Rails.root}/public/404.html", :status => 404 );
+      return;
+    end
+    
+    # Setup then broker and employers variables for the view
+    @broker = @brokerQuery.first;
 		@employers = @broker.employers
 
+    # Support both HTML and Json responses
 	  respond_to do |format|
 		  format.html # index.html.erb
 		  format.json { render json: @broker }
 		end
   end
+
 end
